aaf831e5a236811dc08c6e75dfc44ab94a770f18
awhileback <admin@awhileback.net>
2021-07-27 21:07:34 -0500

    Update urls.py

--- allauth_2fa/urls.py
+++ allauth_2fa/urls.py
@@ -19,3 +19,29 @@ urlpatterns = [
         views.TwoFactorRemove.as_view(),
         name="two-factor-remove"),
 ]
+
+
+"""
+The below code, run in Django shell, can generate a new device
+and give a one time code in the console if an admin gets locked
+out and needs to re-enable a new device. Replace (id=1) with the
+userid for the admin in question. Afterward, the admin should 
+create a new device on their phone, and after verifying it delete 
+the temp device created by the code below.
+"""
+"""
+
+from django_otp.plugins.otp_totp.models import TOTPDevice
+from django_otp.oath import totp
+
+from users.models import CustomUser as User
+
+
+user = User.objects.get(id=1)
+device = TOTPDevice.objects.create(user=user)
+token = totp(device.bin_key)
+
+print('user', user)
+print('6 digit token', token)
+
+"""
